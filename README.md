c158089343049574124813007eb976b947b2ea81
sarakjerstad <saraakjerstad@gmail.com>
2025-06-20 08:41:12 +0200

    First commit, basic setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # CA_nettside
+
+# Firebase Node.js Starter
+
+This project uses:
+- Node.js + Express
+- Firebase Web SDK (Client)
+- Firebase Admin SDK (Backend)
+
+## Setup
+
+```bash
+npm install
---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCK2NAphMT6UEQ4BuZMoh11tOTzgjMe-4E",
+  authDomain: "kda-sommerprosjekt25.firebaseapp.com",
+  projectId: "kda-sommerprosjekt25",
+  storageBucket: "kda-sommerprosjekt25.firebasestorage.app",
+  messagingSenderId: "1079032437259",
+  appId: "1:1079032437259:web:083085009111315fa6050e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
